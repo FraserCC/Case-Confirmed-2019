b1c34b2d1d8764b2641b184468b37892eba2d0a9
Fraser41 <44607939+Fraser41@users.noreply.github.com>
2018-10-31 21:58:54 +0000

    Initial commit

---
+++ CaseConfirmed/Controllers/CasesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaseConfirmed.Models;
+
+namespace CaseConfirmed.Controllers
+{
+    public class CasesController : Controller
+    {
+        // GET: Cases/Random
+        public ActionResult Random()
+        {
+            var MyCase = new Models.Cases() { Subject = "Broken Record" };
+            return  RedirectToAction("Index","Home",new { page = 1, sortBy = "Subject"} );
+        //    return Content("");
+           // return  HttpNotFoundResult();
+    //        return new EmptyResult();
+
+
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("My ID is" + id);
+        }
+        public  ActionResult Index(int? PageIndex, string SortBy)
+        {
+            if (!PageIndex.HasValue)
+            {
+                PageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(SortBy))
+            {
+                SortBy = "Subject";
+            }
+            return Content(String.Format("PageIndex={0}&SortBy={1}", PageIndex, SortBy));
+        }
+    }
+}
---
+++ CaseConfirmed/Models/Cases.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CaseConfirmed.Models
+{
+    public class Cases
+    {
+        public int IntID { get; set; }
+        public string Subject { get; set; }
+        public string CaseNo { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+
+    }
+}
